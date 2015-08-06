863f4763f5f8fa1129277d9d353dd4e9a1c0a8bd
Eric Kelly <heroiceric@gmail.com>
2015-08-06 19:57:46 -0400

    Update ember-cli to v1.13.7

---
+++ ember-cli-build.js
@@ -0,0 +1,20 @@
+/* global require, module */
+var EmberApp = require('ember-cli/lib/broccoli/ember-addon');
+
+module.exports = function(defaults) {
+  var app = new EmberApp(defaults, {
+    // See https://github.com/ember-cli/ember-cli/issues/4084
+    sassOptions: {
+      extension: 'scss'
+    }
+  });
+
+  /*
+    This build file specifes the options for the dummy test app of this
+    addon, located in `/tests/dummy`
+    This build file does *not* influence how the addon or the app using it
+    behave. You most likely want to be modifying `./index.js` or app's build file
+  */
+
+  return app.toTree();
+};
