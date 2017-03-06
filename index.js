f3c74b9ae4efe7d47827bcdfd79cb271f9c21b06
Luke Melia <luke@lukemelia.com>
2017-03-06 17:45:34 -0500

    Drop bower and use bourbon npm module instead.

--- index.js
+++ index.js
@@ -6,16 +6,28 @@ var Funnel = require('broccoli-funnel');
 
 module.exports = {
   name: 'ember-cli-bourbon',
+
   blueprintsPath: function() {
     return path.join(__dirname, 'blueprints');
   },
 
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+
+    // see: https://github.com/ember-cli/ember-cli/issues/3718
+    while (typeof app.import !== 'function' && app.app) {
+      app = app.app;
+    }
+
+    this.bourbonPath = path.dirname(require.resolve('bourbon'));
+    return app;
+  },
+
   treeForStyles: function() {
-    var bourbonPath = path.join(this.project.bowerDirectory, 'bourbon', 'app');
-    var bourbonTree = new Funnel(this.treeGenerator(bourbonPath), {
-      srcDir: '/assets/stylesheets',
-      destDir: '/app/styles'
+    return new Funnel(this.bourbonPath, {
+      srcDir: 'app/assets/stylesheets',
+      destDir: 'app/styles',
+      annotation: 'Funnel (bourbon)'
     });
-    return bourbonTree;
   }
 };
