dec4065b419973c4df6492b48de6659f64b89d50
Luke Melia <luke@lukemelia.com>
2017-03-07 22:17:33 -0500

    Remove reference to bower that I missed

--- blueprints/ember-cli-bourbon/index.js
+++ blueprints/ember-cli-bourbon/index.js
@@ -3,9 +3,5 @@ module.exports = {
     // this prevents an error when the entityName is
     // not specified (since that doesn't actually matter
     // to us
-  },
-
-  afterInstall: function(options) {
-    return this.addBowerPackageToProject('bourbon', '4.2.6');
   }
 };
